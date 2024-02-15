f53252c81c33c28ca10876267bf40142483006fa
Fisher60 <wakefollower@gmail.com>
2024-02-14 22:36:44 -0600

    remove commented code in navbar, add light/dark theming to hero banner

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="w-full pt-12">
-        <img class="w-1/2 mx-auto" src="assets/img/logoVerticalColorLightText.svg" />
+        <ClientOnly>
+            <img v-if="$colorMode.value == 'dark'" class="w-1/2 mx-auto" src="assets/img/logoVerticalColorLightText.svg">
+            <img v-else class="w-1/2 mx-auto" src="assets/img/logoVerticalColor.svg">
+        </ClientOnly>
         <div class="flex flex-row justify-center gap-7 pt-12">
             <button class="btn btn-lg btn-wide btn-primary">Primary</button>
             <button class="btn btn-lg btn-wide btn-outline">Default</button>
--- components/navbar.vue
+++ components/navbar.vue
@@ -2,11 +2,7 @@
     <div class="navbar h-20 fixed bg-primary">
         <div class="flex-1">
             <NuxtLink to="/">
-            <img class="h-8" src="assets/img/logoHorizontalDarkMode.svg" />
-                <!-- <ClientOnly>
-                    <img v-if="$colorMode.value == 'dark'" class="h-10" src="assets/img/logoHorizontalDarkMode.svg">
-                    <img v-else class="h-10" src="assets/img/logoHorizontalLightMode.svg">
-                </ClientOnly> -->
+                <img class="h-8" src="assets/img/logoHorizontalDarkMode.svg" />
             </NuxtLink>
         </div>
 
