ae2a4031f0376c07fb26923378a8d3de237d202f
Fisher60 <wakefollower@gmail.com>
2024-02-15 00:10:23 -0600

    add hover effect buttons to navbar, refactor project pages to directory, remove text selection from hero banner

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="w-full pt-12">
         <ClientOnly>
-            <img v-if="$colorMode.value == 'dark'" class="w-1/2 mx-auto" src="assets/img/logoVerticalColorLightText.svg">
-            <img v-else class="w-1/2 mx-auto" src="assets/img/logoVerticalColor.svg">
+            <img v-if="$colorMode.value == 'dark'" class="w-1/2 select-none mx-auto" src="assets/img/logoVerticalColorLightText.svg">
+            <img v-else class="w-1/2 select-none mx-auto" src="assets/img/logoVerticalColor.svg">
         </ClientOnly>
         <div class="flex flex-row justify-center gap-7 pt-12">
             <button class="btn btn-lg btn-wide btn-primary">Primary</button>
--- components/navbar.vue
+++ components/navbar.vue
@@ -2,13 +2,31 @@
     <div class="navbar px-7 h-20  fixed bg-primary">
         <div class="flex-1">
             <NuxtLink to="/">
-                <img class="h-8" src="assets/img/logoHorizontalDarkMode.svg" />
+                <img class="h-8 select-none" src="assets/img/logoHorizontalDarkMode.svg" />
             </NuxtLink>
         </div>
 
         <div class="flex-none">
             <ul class="menu menu-horizontal text-lg px-1">
-                <li><NuxtLink to="/projects">Projects</NuxtLink></li>
+
+                <li class="dropdown dropdown-end dropdown-hover">
+                    <div tabindex="0" role="button">Projects</div>
+                        <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-40">
+                            <li><NuxtLink to="/projects">Abandon Auth</NuxtLink></li> 
+                            <li><NuxtLink to="/projects">Minecraft Admin</NuxtLink></li>
+                        </ul>
+                </li>
+
+                <li class="dropdown dropdown-end dropdown-hover">
+                    <div tabindex="0" role="button">Docs</div>
+                        <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-40">
+                            <li><NuxtLink to="/projects">Abandon Auth</NuxtLink></li> 
+                            <li><NuxtLink to="/projects">Minecraft Admin</NuxtLink></li>
+                        </ul>
+                </li>
+
+                <li><NuxtLink to="/projects">About Us</NuxtLink></li>
+
             </ul>
         </div>
 
