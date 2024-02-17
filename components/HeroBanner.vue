af3e71c1c9f8127edc1e57db28e42c3ae3a8c4cc
Fisher60 <wakefollower@gmail.com>
2024-02-17 11:26:21 -0600

    change banner to use meme slogan

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -2,8 +2,10 @@
     <div class="flex justify-center pt-16">
         <div>
             <div class="text-base-content text-center text-8xl">
-                <h1>Where Innovation</h1>
-                <h1>Meets Implementation</h1>
+                <!-- <h1>Technology Without</h1>
+                <h1>The Baggage</h1> -->
+                <h1>{{ toHtmlComment("TODO:") }}</h1>
+                <h1>{{ toHtmlComment("Write a Good Slogan") }}</h1>
             </div>
             <div class="flex gap-7 justify-center mt-16">
                 <button class="btn btn-lg btn-wide btn-primary">Learn More</button>
@@ -12,3 +14,9 @@
         </div>
     </div>
 </template>
+
+<script setup>
+    function toHtmlComment(txt) {
+        return `<!-- ${txt} -->`
+    }
+</script>
