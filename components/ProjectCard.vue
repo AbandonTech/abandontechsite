f653a64b54bb3d5e5ce66645c97c8af75a10a806
Fisher60 <wakefollower@gmail.com>
2024-02-25 14:32:56 -0600

    add abandon auth description, add link and hover effects to project cards

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col lg:flex-row justify-between items-center border rounded-md w-11/12 md:w-1/2 mx-auto mt-12 p-12">
+    <NuxtLink :to="props.externalLink" target="_blank" class="bg-base-100 flex flex-col lg:flex-row justify-between items-center border rounded-md w-11/12 md:w-1/2 mx-auto mt-12 p-12 duration-300 hover:scale-110">
         <div class="w-full lg:w-1/2">
             <h3 class="text-base-content text-center text-4xl">{{ props.title }}</h3>
             <img class="w-full mt-4" :src="props.imagePath" />
@@ -7,13 +7,14 @@
 
         <p class="w-full text-lg lg:w-1/2 p-4 lg:p-0 lg:pl-10">{{ props.description }}</p>
         
-    </div>
+    </NuxtLink>
 </template>
 
-<script setup>
+<script setup >
     const props = defineProps({
         title: String,
         imagePath: String,
-        description: String
+        description: String,
+        externalLink: String
     })
 </script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -9,13 +9,16 @@
         minecraftAdmin: {
             title: "Minecraft Admin",
             imagePath: "/img/logoVerticalColorLightText.svg",
+            externalLink: "https://mc.abandontech.cloud",
             description: `Minecraft Admin is a web application for managing your Minecraft servers and giving users fine-grained
             permissions for executing RCON commands on your servers. It also features a proxy for protecting each server's IP address.`
         },
         abandonAuth: {
             title: "Abandon Auth",
             imagePath: "/img/logoVerticalColorLightText.svg",
-            description: `OAuth and Identity Management provider`
+            externalLink: "https://auth.abandontech.cloud",
+            description: `OAuth and Identity Management provider that simplifies user authentication and consolidates user identities
+            accross OAuth providers.`
         }
     }
 </script>
