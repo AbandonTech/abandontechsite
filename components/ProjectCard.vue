b80757866d8db950b269029896e04373e9209bd5
Fisher60 <wakefollower@gmail.com>
2024-02-27 23:43:32 -0600

    add abandon auth sample image, add minecraftadmin image, tweak image size for project cards, add minecraft router project

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,8 +1,8 @@
 <template>
-    <NuxtLink :to="props.externalLink" target="_blank" class="bg-base-100 flex flex-col lg:flex-row justify-between items-center border rounded-md w-11/12 md:w-1/2 mx-auto mt-12 p-12 duration-300 lg:hover:scale-110">
+    <NuxtLink :to="props.externalLink" target="_blank" class="bg-base-100 text-base-content flex flex-col lg:flex-row justify-center lg:justify-between items-center border border-base-content/20 rounded-2xl w-11/12 lg:w-1/2 h-[80vh] lg:h-80 mx-auto mt-12 p-12 duration-300 lg:hover:scale-110">
         <div class="w-full lg:w-1/2">
-            <h3 class="text-base-content text-center text-4xl">{{ props.title }}</h3>
-            <img class="w-full mt-4" :src="props.imagePath" />
+            <h3 class="text-center text-4xl">{{ props.title }}</h3>
+            <img class="w-1/3 mx-auto mt-4" :src="props.imagePath" />
         </div>
 
         <p class="w-full text-lg lg:w-1/2 p-4 lg:p-0 lg:pl-10">{{ props.description }}</p>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <ProjectCard v-bind="projectCardProps.minecraftAdmin" />
     <ProjectCard v-bind="projectCardProps.abandonAuth" />
+    <ProjectCard v-bind="projectCardProps.minecraftAdmin" />
+    <ProjectCard v-bind="projectCardProps.minecraftRouter" />
     <div class="mt-12" />
 </template>
 
@@ -8,17 +9,24 @@
     const projectCardProps = {
         minecraftAdmin: {
             title: "Minecraft Admin",
-            imagePath: "/img/logoVerticalColorLightText.svg",
+            imagePath: "/img/minecraftAdminLogo.svg",
             externalLink: "https://mc.abandontech.cloud",
             description: `Minecraft Admin is a web application for managing your Minecraft servers and giving users fine-grained
             permissions for executing RCON commands on your servers. It also features a proxy for protecting each server's IP address.`
         },
         abandonAuth: {
             title: "Abandon Auth",
-            imagePath: "/img/logoVerticalColorLightText.svg",
+            imagePath: "/img/padlock.svg",
             externalLink: "https://auth.abandontech.cloud",
             description: `OAuth and Identity Management provider that simplifies user authentication and consolidates user identities
             accross OAuth providers.`
+        },
+        minecraftRouter: {
+            title: "Minecraft Router",
+            imagePath: "/img/minecraftAdminLogo.svg",
+            externalLink: "https://github.com/AbandonTech/minecraftrouter",
+            description: `Microservice for resolving DNS to Minecraft servers while providing privacy for server owners by protecting
+                their IP address from connecting users.`
         }
     }
 </script>
