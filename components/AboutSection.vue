8c8dcb472ce6111989bf5ba4de3432b4ada93809
Fisher60 <wakefollower@gmail.com>
2024-03-02 20:31:10 -0600

    small padding and width refactor for better visuals

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center py-12 lg:py-28 text-base-content">
-        <div class="w-full md:w-1/2 px-4 md:px-32 mx-auto">
+        <div class="w-full md:w-2/3 px-4 md:px-20 mx-auto">
             <h1 class="text-5xl lg:text-6xl mb-4">{{ props.title }}</h1>
             <img class="mx-auto w-64 lg:w-80 my-14" :src="props.imagePath" />
             <p class="text-2xl">{{ props.description }}</p>
