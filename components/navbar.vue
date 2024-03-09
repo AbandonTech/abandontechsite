2abfd1c1a0fde21859b1c5d33b35d477f6dc9dc1
Fisher60 <wakefollower@gmail.com>
2024-03-08 23:28:01 -0600

    add sample about and service pages, refactor pages to only use one index.vue, remove dropdowns from navbar
    The dropdown menus were very clunky and overall just not a good experience, so I have removed them. The project structure was confusing with multiple index.vue files in the pages, so these have been refactored to be named after the respective pages.

--- components/navbar.vue
+++ components/navbar.vue
@@ -9,24 +9,9 @@
         <div class="flex-none">
             <ul class="menu menu-horizontal text-lg px-1">
 
-                <li class="dropdown dropdown-end dropdown-hover">
-                    <div tabindex="0" role="button"><NuxtLink to="/projects" @click="removeDropdownFocus">Projects</NuxtLink></div>
-                        <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 text-base-content rounded-box w-40">
-                            <li><NuxtLink to="/projects/abandonauth" @click="removeDropdownFocus">Abandon Auth</NuxtLink></li> 
-                            <li><NuxtLink to="/projects/minecraftadmin" @click="removeDropdownFocus">Minecraft Admin</NuxtLink></li>
-                        </ul>
-                </li>
-
-                <li class="dropdown dropdown-end dropdown-hover">
-                    <div tabindex="0" role="button"><NuxtLink to="/projects" @click="removeDropdownFocus">Services</NuxtLink></div>
-                        <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 text-base-content rounded-box w-40">
-                            <li><NuxtLink to="/projects" @click="removeDropdownFocus">Hosting</NuxtLink></li> 
-                            <li><NuxtLink to="/projects" @click="removeDropdownFocus">Web Dev</NuxtLink></li>
-                            <li><NuxtLink to="/projects" @click="removeDropdownFocus">Backend</NuxtLink></li>
-                        </ul>
-                </li>
-
-                <li><NuxtLink to="/projects">About Us</NuxtLink></li>
+                <li><NuxtLink to="/projects" @click="removeClickFocus">Projects</NuxtLink></li>
+                <li><NuxtLink to="/services" @click="removeClickFocus">Services</NuxtLink></li>
+                <li><NuxtLink to="/about" @click="removeClickFocus">About Us</NuxtLink></li>
 
             </ul>
         </div>
@@ -35,7 +20,7 @@
 </template>
 
 <script setup>
-    function removeDropdownFocus() {
+    function removeClickFocus() {
         // Hack to close dropdowns when they are clicked
         // Removes focus that is holding element open
         document.activeElement?.blur();
