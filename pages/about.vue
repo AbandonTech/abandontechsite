41e8e25c7d7b1ddda9d8ee1f1a09a14b595d2f9b
Fisher60 <wakefollower@gmail.com>
2024-03-08 23:57:02 -0600

    add initial about us content
    This is not final by any means, we should bike shed and improve text on pages like this.

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,11 @@
 <template>
-    <div>About</div>
+    <h1 class="text-base-content text-center text-4xl p-4 lg:text-7xl">About Us</h1>
+    <p class="mx-auto w-11/12 lg:w-2/3 text-2xl pb-10">
+        We are an experienced and diverse team of industry professionals with a wide range of skills. 
+        All of us share a passion for software developement and delivering products that utlize best practice to solve
+        real-world problems.
+        <br />
+        <br />
+        Our goal is to provide excellent and practical software solutions for all who need them.
+    </p>
 </template>
