e5bf1974b057ebe39d8abcf744878817fda3297f
Fisher60 <wakefollower@gmail.com>
2024-03-08 23:56:13 -0600

    simplify bottom nav to improve appearance

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="btm-nav">
-        <NuxtLink to="/" class="text-primary">
+    <div class="btm-nav border-t border-primary border-opacity-50">
+        <NuxtLink to="/" class="text-primary h-5/6 border-r">
             <font-awesome-icon icon="fa-solid fa-house"></font-awesome-icon>
         </NuxtLink>
 
-        <NuxtLink to="/projects" class="text-primary active">
+        <NuxtLink to="/projects" class="text-primary h-5/6 border-r">
             <font-awesome-icon icon="fa-solid fa-rocket"></font-awesome-icon>
         </NuxtLink>
 
-        <NuxtLink to="/projects" class="text-primary">
+        <NuxtLink to="/services" class="text-primary">
             <font-awesome-icon icon="fa-solid fa-briefcase"></font-awesome-icon>
         </NuxtLink>
 
