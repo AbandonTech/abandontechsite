925503ac9c17d4fe2d0934bec6dbfb677df6e104
Fisher60 <wakefollower@gmail.com>
2024-03-23 14:00:42 -0500

    add buttons to projects cards, add mobile responsiveness to cards

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,13 +1,21 @@
 <template>
-    <NuxtLink :to="props.externalLink" target="_blank" class="bg-base-100 text-base-content flex flex-col lg:flex-row justify-center lg:justify-between items-center border border-base-content/20 rounded-2xl w-11/12 lg:w-1/2 h-[80vh] lg:h-80 mx-auto mt-12 p-4 lg:p-12 duration-300 lg:hover:scale-110">
+    <div class="bg-base-100 text-base-content flex flex-col lg:flex-row justify-center lg:justify-between items-center border border-base-content/20 rounded-2xl w-11/12 lg:w-1/2 h-[70vh] lg:h-80 mx-auto mt-12 p-4 lg:p-0">
         <div class="w-full lg:w-1/2">
             <h3 class="text-center text-4xl">{{ props.title }}</h3>
             <img class="w-1/3 mx-auto mt-4" :src="props.imagePath" />
         </div>
 
-        <p class="w-full text-2xl lg:w-1/2 p-4 lg:p-0 lg:pl-10">{{ props.description }}</p>
+        <div class="flex flex-col w-full sm:h-full gap-10 justify-center lg:w-1/2">
+            <p class="text-lg lg:w-10/12 text-center mt-4 sm:mt-0 mx-auto">{{ props.description }}</p>
+            <div class="flex flex-col lg:flex-row gap-4 items-center justify-center">
+                <NuxtLink :to="`/projects/$props.internalLink`"><button type="button" class="btn btn-md w-64 sm:w-32 xl:w-36 2xl:w-48 btn-primary">Learn More</button></NuxtLink>
+                <NuxtLink :to="props.externalLink"><button type="button" class="btn btn-md w-64 sm:w-32 xl:w-36 2xl:w-48 btn-outline">{{ props.title }}</button></NuxtLink>
+            </div>
+            
+        </div>
         
-    </NuxtLink>
+        
+    </div>
 </template>
 
 <script setup >
@@ -15,6 +23,7 @@
         title: String,
         imagePath: String,
         description: String,
-        externalLink: String
+        externalLink: String,
+        internalLink: String
     })
 </script>
