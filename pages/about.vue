863f93098d155ab50fbd6ae005ed57a262fbb155
Fisher60 <wakefollower@gmail.com>
2024-04-07 13:11:19 -0500

    add mission statement to about

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,32 @@
 <template>
-    <h1 class="text-base-content text-center text-4xl p-4 lg:text-7xl">About Us</h1>
-    <p class="mx-auto w-11/12 lg:w-2/3 text-2xl pb-10">
-        We are an experienced and diverse team of industry professionals with a wide range of skills. 
-        All of us share a passion for software developement and delivering products that utlize best practice to solve
-        real-world problems.
-        <br />
-        <br />
-        Our goal is to provide excellent and practical software solutions for all who need them.
-    </p>
+    <div class="text-base-content lg:mt-10 pb-10 lg:pb-20 w-full">
+        <h1 class="text-base-content text-center text-4xl p-8 lg:text-7xl">About Us</h1>
+        <p class="mx-auto w-11/12 lg:w-2/3 text-2xl">
+            We are an experienced and diverse team of industry professionals with a wide range of skills.
+            All of us share a passion for software developement and delivering products that utlize best practice to solve
+            real-world problems.
+            <br />
+            <br />
+            Most of us are career software engineers and career learners.
+        </p>
+    </div>
+    
+
+    <div class="text-base-content  pb-10 lg:pb-20 w-full">
+        <h1 class=" text-center pb-4 text-4xl lg:text-7xl">Our Mission</h1>
+        <div class="w-11/12 lg:w-2/3 mx-auto">
+            <p class="text-2xl">
+            Our mission comes in two parts:
+                <ul class="text-2xl list-disc p-6 lg:px-10">
+                    <li>
+                        To provide excellent and practical software solutions for all who need them
+                    </li>
+                    <li class="pt-4">
+                        Improve the community via open source contributions and making our own codebases open source whenever possible
+                    </li>
+                </ul>
+            </p>
+        </div>
+    </div>
+    
 </template>
