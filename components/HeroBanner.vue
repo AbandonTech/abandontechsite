f73f808d8179de39b65a7b87ea42c89e5251d702
Fisher60 <wakefollower@gmail.com>
2024-04-07 13:29:57 -0500

    make mobile abandontech logo on herobanner responsive to light/dark mode

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -5,7 +5,12 @@
                 <h1 v-if="debug" class="-rotate-2">&#60;!-- <span class="underline underline-offset-8 decoration-wavy decoration-[5px] decoration-amber-300">TODO:</span> Write a Good Slogan --&#62;</h1>
                 <template v-else>
                     <h1 class="hidden lg:block text-4xl lg:text-9xl mb-10">AbandonTech</h1>
-                    <img class="block lg:hidden w-full mx-auto mb-10" src="/img/logoVerticalColorLightText.svg" />
+                    <ClientOnly>
+                        <div class="block lg:hidden w-full mx-auto mb-10">
+                            <img v-if="$colorMode.value == 'dark'" src="/img/logoVerticalColorLightText.svg">
+                            <img v-else src="/img/logoVerticalColor.svg">
+                        </div>
+                    </ClientOnly>
                     <div class="divider divider-primary mb-10"></div>
 
                     <h1>Empowering Businesses</h1>
