597a2ab82c81cfb40fcf6e58b9a5a7d9aa48126a
Fisher60 <wakefollower@gmail.com>
2024-04-07 13:19:08 -0500

    add abandontech logo on mobile banner

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -4,9 +4,8 @@
             <div class="text-base-content text-center text-4xl p-4 lg:text-7xl">
                 <h1 v-if="debug" class="-rotate-2">&#60;!-- <span class="underline underline-offset-8 decoration-wavy decoration-[5px] decoration-amber-300">TODO:</span> Write a Good Slogan --&#62;</h1>
                 <template v-else>
-                    <!-- <img class="w-full mx-auto mb-10" src="/img/logoVerticalColorLightText.svg" /> -->
-                    <h1 class="text-9xl mb-10">AbandonTech</h1>
-
+                    <h1 class="hidden lg:block text-4xl lg:text-9xl mb-10">AbandonTech</h1>
+                    <img class="block lg:hidden w-full mx-auto mb-10" src="/img/logoVerticalColorLightText.svg" />
                     <div class="divider divider-primary mb-10"></div>
 
                     <h1>Empowering Businesses</h1>
