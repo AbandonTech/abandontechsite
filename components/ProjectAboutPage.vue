fa692ddca2b0db4070885fe4461f020eca61630c
Fisher60 <wakefollower@gmail.com>
2024-04-07 23:27:03 -0500

    add component for about projects pages, add abandon auth project page

---
+++ components/ProjectAboutPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col justify-center py-20 lg:py-20">
+        <div class="text-base-content text-center text-4xl p-4 lg:text-7xl">
+            <h1 class="hidden lg:block text-4xl lg:text-9xl mb-10">{{ props.title }}</h1>
+        </div>
+        <div class="self-center w-11/12 lg:w-2/3 mb-10">
+            <p class="text-primary-content text-xl whitespace-pre-line">
+                {{ props.description }}
+            </p>
+        </div>
+        <NuxtLink :to="props.externalLink" class="self-center" target="_blank" rel="noreferrer"><button type="button" class="btn btn-md w-64 btn-outline">{{ props.title }} <font-awesome-icon icon="fa-solid fa-arrow-up-right-from-square" /></button></NuxtLink>
+    </div>
+</template>
+
+<script setup >
+    const props = defineProps({
+        title: String,
+        description: String,
+        externalLink: String,
+    })
+</script>
--- pages/projects/AbandonAuth.vue
+++ pages/projects/AbandonAuth.vue
@@ -1,3 +1,17 @@
 <template>
-    <div>Abandon Auth</div>
+    <ProjectAboutPage v-bind="abandonAuthProps" />
 </template>
+
+<script setup>
+    const abandonAuthProps = {
+        title: "Abandon Auth",
+        externalLink: "https://auth.abandontech.cloud",
+        description: `AbandonAuth is your premier authentication and identity management solution designed to streamline the authentication process for your web applications.
+        
+            AbandonAuth specializes in simplifying authentication by leveraging a diverse array of third-party providers. This approach significantly reduces the coding burden and effort needed to implement authentication within your applications.
+
+            By entrusting us with the intricate aspects of authentication, you can redirect your focus towards refining the core functionalities of your projects. Rest assured, your users' security remains our utmost priority as they seamlessly authenticate through their preferred providers.
+            
+            Experience hassle-free authentication with AbandonAuth, empowering you to prioritize your project's growth and innovation while ensuring your users' security.`
+    }
+</script>
