5edd435b8f7e9728bbb291065f00f6e26ddbeb33
Fisher60 <wakefollower@gmail.com>
2024-04-07 23:49:08 -0500

    fix mobile responsiveness on project about pages

--- components/ProjectAboutPage.vue
+++ components/ProjectAboutPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col justify-center py-20 lg:py-20">
         <div class="text-base-content text-center text-4xl p-4 lg:text-7xl">
-            <h1 class="hidden lg:block text-4xl lg:text-9xl mb-10">{{ props.title }}</h1>
+            <h1 class="text-4xl lg:text-9xl mb-10">{{ props.title }}</h1>
         </div>
         <div class="self-center w-11/12 lg:w-2/3 mb-10">
             <p class="text-primary-content text-xl whitespace-pre-line">
