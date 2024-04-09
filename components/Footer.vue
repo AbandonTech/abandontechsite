b44a16c62e8e1492627a5557efac79fffe327e98
Fisher60 <wakefollower@gmail.com>
2024-04-09 10:04:04 -0500

    use client only for footer and bottom nav due to partial hydration on font awesome icons

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,8 +2,10 @@
     <footer id="footer" class="footer h-64 lg:h-full p-10 bg-primary text-primary-content">
         <div class="flex flex-col items-center text-2xl gap-4 mx-auto">
             <div class="flex flex-row gap-3">
-                <NuxtLink to="https://github.com/AbandonTech" target="_blank"><font-awesome-icon icon="fa-brands fa-github" /></NuxtLink>
-                <NuxtLink to="mailto:info@abandontech.cloud" target="_blank"><font-awesome-icon icon="fa-solid fa-envelope" /></NuxtLink>
+                <ClientOnly>
+                    <NuxtLink to="https://github.com/AbandonTech" target="_blank"><font-awesome-icon icon="fa-brands fa-github" /></NuxtLink>
+                    <NuxtLink to="mailto:info@abandontech.cloud" target="_blank"><font-awesome-icon icon="fa-solid fa-envelope" /></NuxtLink>
+                </ClientOnly>
             </div>
             <p>info@abandontech.cloud</p>
         </div>
