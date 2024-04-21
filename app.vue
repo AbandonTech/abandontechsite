9e6348b612a90cb80c40f451f06cc3dfe5e44c6d
Fisher60 <wakefollower@gmail.com>
2024-04-20 23:06:15 -0500

    add userSeoMeta to create custom embed

--- app.vue
+++ app.vue
@@ -16,4 +16,14 @@
       class: "bg-base-100 text-primary-content"
     }
   })
+
+  useSeoMeta({
+    title: "AbandonTech",
+    ogTitle: "AbandonTech",
+    description: "Empowering Businesses Through Technology",
+    ogDescription: "Empowering Businesses Through Technology",
+    ogImage: "/img/logoVerticalColor.svg",
+    twitterCard: "summary_large_image",
+    themeColor: "#1A67D7"
+})
 </script>
