fc39824b7770a596f7897b5f65867eeae9924277
Fisher60 <wakefollower@gmail.com>
2024-07-07 22:42:26 -0500

    add aria-label for links without text
    This is for accessibility to allow screen readers to read link information when links do not contain readable text.

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="btm-nav border-t border-primary border-opacity-50">
         <ClientOnly>
-            <NuxtLink to="/" class="text-primary h-5/6 border-r">
+            <NuxtLink to="/" class="text-primary h-5/6 border-r" aria-label="Navigate to the home page">
                 <font-awesome-icon icon="fa-solid fa-house"></font-awesome-icon>
             </NuxtLink>
 
-            <NuxtLink to="/projects" class="text-primary h-5/6 border-r">
+            <NuxtLink to="/projects" class="text-primary h-5/6 border-r" aria-label="Navigate to the projects page">
                 <font-awesome-icon icon="fa-solid fa-rocket"></font-awesome-icon>
             </NuxtLink>
 
-            <NuxtLink to="/about" class="text-primary">
+            <NuxtLink to="/about" class="text-primary" aria-label="Navigate to the about page">
                 <font-awesome-icon icon="fa-solid fa-briefcase"></font-awesome-icon>
             </NuxtLink>
         </ClientOnly>
