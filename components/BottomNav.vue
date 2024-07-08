f87f6c725f16f1dcad32953f37d0ba004cff2efc
Fisher60 <wakefollower@gmail.com>
2024-07-08 17:24:03 -0500

    remove fortawesome plugin and replace fortawesome with svg icons
    This is a massive performance increase according to lighthouse (around a 70% decrease in load times for largest contentful paint from 10 seconds to 3). This should offer us a large performance boost when looking at SEO metrics.

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -2,15 +2,15 @@
     <div class="btm-nav border-t border-primary border-opacity-50">
         <ClientOnly>
             <NuxtLink to="/" class="text-primary h-5/6 border-r" aria-label="Navigate to the home page">
-                <font-awesome-icon icon="fa-solid fa-house"></font-awesome-icon>
+                <svg class="h-4 fill-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
             </NuxtLink>
 
             <NuxtLink to="/projects" class="text-primary h-5/6 border-r" aria-label="Navigate to the projects page">
-                <font-awesome-icon icon="fa-solid fa-rocket"></font-awesome-icon>
+                <svg class="h-4 fill-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2v82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9V380.8c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/></svg>
             </NuxtLink>
 
             <NuxtLink to="/about" class="text-primary" aria-label="Navigate to the about page">
-                <font-awesome-icon icon="fa-solid fa-briefcase"></font-awesome-icon>
+                <svg class="h-4 fill-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z"/></svg>
             </NuxtLink>
         </ClientOnly>
     </div>
