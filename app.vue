b749dbbb64fa8e9bde3c04b933783159ebc682eb
Kyler <39353605+fisher60@users.noreply.github.com>
2024-07-11 11:06:24 -0500

    Analytics/add google analytics (#15)
    * add google analytics configuration
    
    * remove 'type' attribute from preloaded font link elements in head. The 'type' attribute is not valid, this was a config found from stack overflow and I cannot find any reference for why the 'type' attribute would be included.

--- app.vue
+++ app.vue
@@ -21,13 +21,11 @@ const runtimeConfig = useRuntimeConfig()
     link: [
     {
       rel: "preload",
-      type: "font/Atkinson Hyperlegible",
       href: "https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&display=swap",
       as: "font",
     },
     {
       rel: "preload",
-      type: "font/Lato",
       href: "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap",
       as: "font",
     }
